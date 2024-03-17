99b1bd08697d6896a76b23576d7bee6b1a140853
evelynnn04 <yosianaevelyn5@gmail.com>
2024-03-17 16:37:23 +0700

    fix input

--- src/PYTHON/bezier.py
+++ src/PYTHON/bezier.py
@@ -12,85 +12,6 @@ num_of_point = app.input_value("banyak pasangan titik")
 for i in range(num_of_point):
     app.input_point(i, list_of_point, list_of_x, list_of_y)
 
-<<<<<<< HEAD
-def connect(list1,list2):
-    if(len(list1) == 0):
-        return list2
-    else:
-        list2.pop(0)
-        return list1 + (list2)
-
-def connect1(list1,list2):
-    if(len(list1) == 0):
-        return list2
-    else:
-        return list1 + (list2)
-
-# def divide(n,list):
-#     if(len(list) == n):
-#         return list
-#     else:
-#         mid = len(list)//2
-#         left = [divide(n, list[:mid+1])]
-#         right = [divide(n, list[mid:])]
-#         result = connect1(left,right)
-#         print(result)
-#         return result
-
-def general_iterate(n, iterate, count, list):
-    if(iterate == count):
-        return list
-    else:
-        result = []
-        for i in range(2**count):
-            temp = []
-            for j in range(n):
-                temp.append(list[(n-1)*i+j])
-            temp = base_iterate(n,temp)
-            result = connect(result,temp)
-        return general_iterate(n,iterate,count+1,result)
-
-def take_result_point(list, n):
-    result = []
-    for i in range(len(list)):
-        if(i%(n-1) == 0):
-            result.append(list[i])
-    return result
-
-
-p = (1,2)
-pp = (0,5)
-
-mp = midPoint(p,pp)
-# print(mp)
-
-list = [(0,0)]
-list = wide(p,list,mp)
-# print(list)
-
-d = [[0,0],[2,2],[4,2],[6,2],[8,0]]
-# d = divide(3,d)
-# print(d)
-# print(len(bez))
-
-kr = [[0, 0], [1.0, 1.0], [2.0, 1.5], [3.0, 2.0], [4.0, 2.0]]
-kn = [[4.0, 2.0], [5.0, 2.0], [6.0, 1.5], [7.0, 1.0], [8, 0]]
-# gb = connect(kr,kn)
-# print(gb)
-
-# bez = base_iterate(3,bez)
-bez = [[238.0, 680.0], [312.8, 0.0], [374.0, 0.0], [442.0, 680.0]]
-
-start_time = time.time()
-bez = general_iterate(4,4,0,bez)
-end_time = time.time()
-t = end_time - start_time
-print(bez)
-print()
-print(take_result_point(bez,4))
-print(len(bez))
-print(t)
-=======
 # Solve
 for point in list_of_point:
     list_of_x.append(point[0])
@@ -101,4 +22,3 @@ result_y = app.brute_force_bezier (num_of_point, list_of_y, itr)
 result_bf = app.merge_xy(result_x, result_y)
 print(result_dnc)
 print(result_bf)
->>>>>>> a90d25875a8ec9d1f15e77b38df1e6080536cb4d
--- src/PYTHON/gui.py
+++ src/PYTHON/gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import turtle
 
 def solve():
+    draw_axes()
+    draw_bezier()
     num_iterations = num_iterations_entry.get()
     num_points = num_points_entry.get()
     list_of_point = list_of_point.get()
@@ -17,8 +19,28 @@ def solve():
         tk.messagebox.showwarning(title="Invalid input", message="Input invalid!")
         return
 
-def start_simulation():
-    pass
+def draw_bezier():
+    list_result = [[90,7], [0,8], [9,4000], [3,4]]
+    draw_curve(list_result)
+
+def draw_curve(points):
+    t.penup()
+    t.goto(points[0][0], points[0][1])
+    t.pendown()
+    t.color("grey")
+    for i in range(1, len(points)):
+        t.goto(points[i][0], points[i][1])
+
+def draw_axes():
+    t.color("lightgrey")
+    t.penup()
+    t.goto(-300, 0)
+    t.pendown()
+    t.goto(300, 0)
+    t.penup()
+    t.goto(0, -300)
+    t.pendown()
+    t.goto(0, 300)
 
 # Initialization
 root = tk.Tk()
@@ -29,16 +51,16 @@ root.configure(bg="#2C2B30")
 root.geometry("920x600")
 root.resizable(False, False)
 
-# Left side: input fields and solve button
-left_frame = tk.Frame(root, bg="#2C2B30", padx=10, pady=10)  # Adjust padding
+# Left side
+left_frame = tk.Frame(root, bg="#2C2B30", padx=10, pady=10) 
 left_frame.pack(side=tk.LEFT)
 
-# Right side: Turtle canvas with margin
+# Right side
 right_frame = tk.Frame(root, bg="#2C2B30", padx=10, pady=10)
-right_frame.pack(side=tk.LEFT)  # Pack after the left frame
+right_frame.pack(side=tk.LEFT)  
 
-left_frame.grid_columnconfigure(1, weight=1)  # Left side occupies 1/3 of width
-right_frame.grid_columnconfigure(0, weight=2)  # Right side occupies 2/3 of width
+left_frame.grid_columnconfigure(1, weight=1)  
+right_frame.grid_columnconfigure(0, weight=2) 
 
 # Num of point
 num_points_label = tk.Label(left_frame, text="Number of Points:", bg="#2C2B30", fg="white", font=("Palatino", 10))
